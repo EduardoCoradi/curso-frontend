98db2eb1040488223d240fafc669bac19d9a38b4
Eduardo Coradi <94080453+EduardoCoradi@users.noreply.github.com>
2021-11-30 16:43:36 -0300

    adicionando regex ao formulario uf

---
+++ Javascript/javascript.js
@@ -0,0 +1,39 @@
+/*## Operadores
+
+## Aritméticos: retornam o resultado de uma operação
+
++ somar
+- subtrair
+* multiplicar
+/ dividir
+% módulo (resto da divisão)
+
+## Comparadores matemáticos: teste lógico, retorno booleano (true/ false):
+
+< menor que
+> maior que
+<= menor ou igual há
+>= maior ou igual há
+
+## Comparadores lógicos: teste lógico, retorno booleano (true / false):
+
+== igualdade entre sentenças (valor)
+!= diferença entre sentenças (valor)
+=== igualdade entre sentenças (valor e tipo)
+!== diferença entre sentenças (valor e tipo)
+
+! no javascript quer dizer negação
+= sozinho (recebe)
+
+## Operadores de lógica e junção lógica
+
+! Não (NOT)
+&& E (AND)
+|| OU (OR)*/
+
+var km
+var revisao = 3
+
+for(km = 0; km < revisao; km++) {
+    console.log('apenas ' + km + 'kms pode rodar')
+}
